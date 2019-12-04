915f62a9ef3152bfceba499a4fd8cd11d93e3560
Arf0r <47351811+Arf0r@users.noreply.github.com>
2019-12-04 17:46:13 +0100

    Update SampleApplication.py

--- SampleApplication.py
+++ SampleApplication.py
@@ -11,7 +11,15 @@ Class that implements the main functionality of the integration robot Howdy.
 class SampleApplication(Base.AbstractApplication):
     def __init__(self):
         # Initiate variables that will be used globally.
-        self.answer = None
+        self.gestureLock = Semaphore(0)
+        self.levelLock = Semaphore(0)
+        self.langLock = Semaphore(0)
+        self.practiceTestLock = Semaphore(0)
+        self.nameLock = Semaphore(0)
+        self.speechLock = Semaphore(0)
+        self.learnTime = Semaphore(0)
+        self.answerLock = Semaphore(0)
+        self.answer = ""
         self.name = ""
         self.practiceTest = ""
         self.level = ""
@@ -33,7 +41,6 @@ class SampleApplication(Base.AbstractApplication):
         self.setRecordAudio(True)
 
         # Set the language to American English
-        self.langLock = Semaphore(0)
         self.setLanguage('en-US')
         self.langLock.release()
 
@@ -43,7 +50,7 @@ class SampleApplication(Base.AbstractApplication):
 
         # Nao says hey, and asks for name.
         self.speechLock = Semaphore(0)
-        self.sayAnimated("Howdy partner! What is your name")
+        self.say("Howdy partner! What is your name")
         self.speechLock.acquire()
 
         # Start introduction sequence.
@@ -51,108 +58,151 @@ class SampleApplication(Base.AbstractApplication):
 
     def introduction(self):
         # Listen for name of the user.
-        self.nameLock = Semaphore(0)
         self.setAudioContext("answer_name")
         self.startListening()
         self.nameLock.acquire(timeout=4)
         self.stopListening()
+        if self.name == "":
+            self.nameLock.acquire(timeout=1)
+
 
         # If no name was recorded keep asking for name until it is acquired:
-        if self.name == "":
-            while self.name == "":
-                self.speechLock = Semaphore(0)
-                self.say("Sorry I didn't catch your name, could you repeat it?")
-                self.speechLock.acquire()
-                self.startListening()
-                self.nameLock.acquire(timeout=4)
-                self.stopListening()
+        while self.name == "":
+            self.say("Sorry I didn't catch your name, could you repeat it?")
+            self.speechLock.acquire()
+            self.setAudioContext("answer_name")
+            self.startListening()
+            self.nameLock.acquire(timeout=4)
+            self.stopListening()
+            self.nameLock.acquire(timeout=1)
+
 
         # If a name was recorded, pick one of the answers and start next function
-        else:
-            print(self.name)
-            self.pickFromList(self.nameList)
-            self.practiceOrTest()
+        print(self.name)
+        self.pickFromList(self.nameList, self.name)
+        self.practiceOrTest()
 
     def practiceOrTest(self):
         # Listen for whether to test or to practice
-        self.practiceTestLock = Semaphore(0)
-        self.setAudioContext("awaiting_practice_test")
+        self.setAudioContext("answer_practice_test")
+        self.setAudioHints("Practice", "test")
         self.startListening()
+
         self.practiceTestLock.acquire(timeout=4)
         self.stopListening()
+        if self.practiceTest == "":
+            print('1')
+            self.practiceTestLock.acquire(timeout=1)
 
         # If no name was recorded keep asking for name until it is acquired:
-        if self.practiceTest == "":
-            while self.practiceTest == "":
-                self.speechLock = Semaphore(0)
-                self.say("Sorry I didn't catch that")
-                self.speechLock.acquire()
-                self.startListening()
-                self.practiceTestLock.acquire(timeout=4)
-                self.stopListening()
+        while self.practiceTest == "":
+            self.say("Sorry I didn't catch that")
+            self.speechLock.acquire()
+            self.setAudioContext("answer_practice_test")
+            self.startListening()
+            self.practiceTestLock.acquire(timeout=4)
+            self.stopListening()
 
-        if self.practiceTest == "practice":
-            self.pickFromList(self.practiceList)
+        # If the user chose to practice, give appropriate response
+        if self.practiceTest == "Practice":
+            self.pickFromList(self.practiceList, "")
             self.whichLevel()
 
-        elif self.practiceTest == "test":
-            self.pickFromList(self.testList)
+        # If user chose to test, give appropriate response
+        elif self.practiceTest == "Test":
+            self.pickFromList(self.testList, "")
             self.whichLevel()
 
     def whichLevel(self):
-        self.levelTestLock = Semaphore(0)
-        if self.practiceTest == "practice":
-            self.setAudioContext("practice-followup")
-        elif self.practiceTest == "test":
-            self.setAudioContext("test-followup")
-        self.startListening()
-        self.levelTestLock.acquire(timeout=3)
-        self.stopListening()
-        self.level = int("1")
+        # Set the right audio context in Dialogflow, depending on whether user chose to test or practice
+        self.listenToLevel()
 
+        # If answer was recorded give response and start practice or test sequence
         if self.level:
-            self.pickFromList(self.levelList)
-
-        if self.practiceTest == "practice":
+            self.pickFromList(self.levelList, "")
+        if self.practiceTest == "Practice":
             self.practice()
-        elif self.practiceTest == "test":
+        elif self.practiceTest == "Test":
             self.test()
 
+    def listenToLevel(self):
+        # Listen for what level the user wants to test or practice
+        self.setAudioContext("answer_level")
+        self.startListening()
+        self.levelLock.acquire(timeout=3)
+        self.stopListening()
+
+        # Keep prompting user for answer while no answer has been recognised
+        while self.level == "":
+            self.say("Sorry I didn't catch that")
+            self.setAudioContext("answer_level")
+            self.speechLock.acquire()
+            self.startListening()
+            self.levelLock.acquire(timeout=4)
+            self.stopListening()
+
     def practice(self):
-        wordDict = self.wordDicts[self.level]
-        self.sayAnimated(
-            "Lets practice!, I will first say a word in english and then repeat it in Syrian, then I'll give you a couple seconds to repeat the word")
-        time.sleep(6)
+        # Load the word dictionary that corresponds to the level that was chosen
+        wordDict = self.wordDicts[int(self.level)]
+
+        # Tell the user how the practice session will work
+        self.sayAnimated("Lets practice!, I will first say a word in english and then repeat it in Dutch, "
+                         "then I'll give you a couple seconds to repeat the word")
+        self.speechLock.acquire()
+
+        # For every word pair in the dictionary run the practiceWord function
         numberWord = 0
         for key, value in wordDict.items():
             self.practiceWord(key, value, numberWord)
             numberWord += 1
+            print(numberWord)
 
+        # When the session is over, give a compliment and ask if the user want to test their new skills
         self.sayAnimated("Nice practice session brosky, you're the best in the wild west!")
         self.sayAnimated("Do you want to Test your new skills?")
 
+        # Save the user and the practice session in the database
         self.savePracticeLevel(self.level)
 
     def practiceWord(self, key, value, numberWord):
-        self.learnTime = Semaphore(0)
-        self.learnTime.acquire(timeout=1)
+        # Reset the language to English and say the key word
         self.setLanguage("en-US")
+        self.langLock.acquire()
         self.say(key)
         self.learnTime.acquire(timeout=1)
-        self.setLanguage("nl-NL")
         self.say("means")
-        self.learnTime.acquire(timeout=0.5)
+
+        # Set the language to Dutch and say the practice word
+        # self.setLanguage("nl-NL")
+        # self.langLock.acquire()
         self.say(value)
         self.learnTime.acquire(timeout=1)
+
+        # On the first couple words tell the user to repeat the word, then just wait for the user to repeat
         if numberWord < 3:
             self.sayAnimated("repeat please")
         self.learnTime.acquire(timeout=5)
+
         # May implement a method that recognises the input from user and checks if the pronunciation is correct.
 
     def test(self):
         """
         Here the robots starts to test the user by looping through all the words to test.
             """
+
+        levels = self.retrievePracticeLevel()
+        if len(levels) == 0:
+            self.sayAnimated(self.name + ", you need to practice levels first!")
+            self.practice()
+        else:
+            self.sayAnimated("Would you like to test level")
+            firstLoop = True
+            for level in levels:
+                if not firstLoop:
+                    self.sayAnimated(" or ")
+                self.sayAnimated(str(level))
+                firstLoop = False
+            self.listenToLevel()
 
         wordDict = self.wordDicts[self.level]
         self.sayAnimated("Alright let's test your skills")
@@ -162,11 +212,12 @@ class SampleApplication(Base.AbstractApplication):
         for key, value in wordDict.items():
             self.testWord(key, value, counter, wrongWords)
             counter += 1
+
         # Here the word that the user got wrong are tested. It removes te word from the list of tuples if the user
         # has it right. It will continue to ask the wrong words (by using the while loop) until there are no more
         # words in the list.
         while len(wrongWords) > 0:
-            # we are using a copy to loop through, otherwises it loops through the list while items are being
+            # we are using a copy to loop through, otherwise it loops through the list while items are being
             # removed. This makes the code skip some elements of the list.
             for key, value in wrongWords[:]:
                 self.testWord(key, value, counter, wrongWords)
@@ -174,62 +225,77 @@ class SampleApplication(Base.AbstractApplication):
     def testWord(self, key, value, counter, wrongWords):
         """Tests a single word by giving the word the user is supposed to translate, and to listen to the input of
         user, checking if the given word corresponds to the answer """
+        self.setLanguage("en-US")
+        self.langLock.acquire()
         if counter < 3:
             # After pronouncing three words, exclude the "What does... mean from the speech of the robot. "
             self.sayAnimated("What does" + key + "mean?")
         else:
             self.sayAnimated(key)
+
+        # Listen for answer
+        # self.setLanguage("nl-NL")
+        # self.langLock.acquire()
+        self.setAudioContext("answer_answerLevelOne")
         self.startListening()
         self.answerLock.acquire(timeout=4)
         self.stopListening()
+
         if self.answer:
             if value == self.answer:
                 # If the given value by the user is right, say it is right and remove it to the wrongWords list.
-                self.sayAnimated(self.pickFromList(self.complimentList))
+                self.sayAnimated(self.pickFromList(self.complimentList, ""))
                 if value in [v for k, v in wrongWords]:
                     wrongWords.remove((key, value))
 
-
             else:
                 # If the given value by the user is wrong, say it is wrong and append it to the wrongWords list.
-                self.sayAnimated(self.pickFromList((self.wrongAnswerList)))
+                self.sayAnimated(self.pickFromList((self.wrongAnswerList, "")))
                 wrongWords.append((key, value))
         else:
             self.sayAnimated("speak up you shy bitch")
 
     def loadDicts(self):
-        self.wordDicts = {
-            1: {"hello": "marhabaan", "goodbye": "wadaeaan", "job": "wazifa", "wages": "ujur", "salary": "ratib",
-                "work": "eamal", "goal": "hadaf", "colleague": "zamil", "skills": "maharat", "manager": "mudir",
-                "manage": "tadbir", "company": "sharika", "task": "muhima", "interview": "muqabala"}
-            ,
-            2: {"business": "aemal", "wholesale": "bialjumla", "sales": "mabieat", "achieve": "altawasul",
-                "venture": "almaghamir", "brand": "ealamat tijaria", "delegation": "wafda", "meet": "yajtamie",
-                "talk": "hadith", "busy": "mashghul", "delivery": "tawsil", "order": "talab", "know": "aerf",
-                "join": "aindama"}
-            ,
-            3: {"entrepreneur": "ryady", "dedication": "tafan", "citizenship": "almuatina", "citizen": "muatin",
-                "expect": "tuaqie", "learn": "taealam", "wish": "raghba", "effort": "majhud", "offer": "eard",
-                "discuss": "munaqasha", "allow": "alsamah", "areas": "almanatiq", "resume": "sirat dhatia",
-                "employment": "tawzif"}
-            ,
-            4: {"catering industry": "sinaeat altaeam", "working hours": "saeat aleamal",
-                "civil servant": "iinaa aistifan", "police officer": "dabit shurta", "fireman": "rajul al'iitfa",
-                "retail": "altajzia", "hospital": "mustashfaa", "full - time": "waqt kamil", "part - time":
-                    "dawam jazyaa", "tax": "hamal"}
-            ,
-            5: {"drivers license": "rukhsat alssayiq", "apply for": "altaqadum bitalab lilhusul",
-                "responsibilities": "almaswuwliat", "vacancy": "shaghir", "vocational level": "almustawaa almahniu",
-                "cover letter": "ghita' alrisala", "life insurance": "altaamin ealaa alhaya",
-                "tax advisor": "mustashar aldarayib", "investing": "alaistithmar"}
+        """
+        Loads dictionaries of translated word pairs into another dictionary that can be used globally
+            """
+        self.wordDicts= {
+            1: {"hello": "hallo", "goodbye": "doei", "job":"baan", "wages": "loon", "salary": "salaris",
+                "work": "werk", "goal": "doel", "colleague": "collega", "skills": "vaardigheden",
+                 "manager": "manager", "manage": "beheren", "company": "bedrijf", "task": "taak",
+                "interview": "sollicitatie"},
+
+            2: {"business’":"zaken", "wholesale": "groothandel", "sales": "verkoop", "achieve": "bereiken",
+                "venture": "onderneming", "brand": "merk", "delegation": "delegatie", "meet": "afspreken",
+                "talk": "praten", "busy": "bezig", "delivery": "bezorging", "order": "bestellen", "know": "weten",
+                "join": "meedoen"},
+
+            3: {"entrepreneur": "ondernemer", "dedication": "toewijding", "citizenship": "burgerschap",
+                "citizen": "burger", "expecting": "verwachten", "learning": " leren", "wishing": " wensen",
+                "effort": "inspanning", "offer": " aanbod ", "discuss":  "bespreken", "allow": "toestaan",
+                "areas": "gebieden", "resume": "hervatten", "employment": "werkgelegenheid"},
+
+            4: {"catering industry": "horeca", "working hours": "werkuren", "civil servant": "ambtenaar",
+                "police officer": "politie agent", "fireman": "brandweerman", "retail": "kleinhandel",
+                "hospital":"ziekenhuis", "full - time": "voltijd", "part - time": "deeltijd", "tax": "belasting"},
+
+            5: {"drivers license": "rijbewijs", "apply for ": "solliciteren voor",
+                "responsibilities": "verantwoordelijkheden", "vacancy":"vacature", "vocational level": "beroepsniveau",
+                "cover letter": "voorblad", "life insurance": "levensverzekering", "tax advisor": "belastingadviseur",
+                " investing": "beleggen"}
         }
 
-    def pickFromList(self, list):
+    def pickFromList(self, list, var):
+        print(self.nameList)
         randint = random.randint(0, len(list) - 1)
         self.speechLock = Semaphore(0)
-        self.sayAnimated(list[randint])
+        print(list[randint])
+        self.sayAnimated(list[randint].format(var))
         self.speechLock.acquire()
 
+    def Gesture(self, id):
+        self.doGesture(id)
+        self.gestureLock.release()
 
     def listen(self):
         self.startListening()
@@ -237,11 +303,11 @@ class SampleApplication(Base.AbstractApplication):
         self.stopListening()
 
     def loadLists(self):
-        self.nameList = [self.name + "My friend, do you want to practice with me or should I take a test",
-                         "Nice to meet you" + self.name + "my name is Howdy! Would you like to practice or take a test?",
-                         "My name is Howdy, saddle up" + self.name + " Would you like to practice or take a test?",
-                         "yeeee haww, nice to meet you " + self.name + ", I'm Howdy! Would you like to practice or take a test?",
-                         "Hey " + self.name + ", Good to see you. Do you want to practice or take a test?"]
+        self.nameList = ["{} My friend, do you want to practice with me or should I take a test",
+                         "Nice to meet you {} my name is Howdy! Would you like to practice or take a test?",
+                         "My name is Howdy, saddle up {} Would you like to practice or take a test?",
+                         "yee haw, nice to meet you {} , I'm Howdy! Would you like to practice or take a test?",
+                         "Hey {}, Good to see you. Do you want to practice or take a test?"]
 
         self.practiceList = ["Okay! Which level do you want to practice? There are 5 levels.",
                              "Alright! Lets do that, There are 5 Levels, which level do you want to practice?",
@@ -254,6 +320,8 @@ class SampleApplication(Base.AbstractApplication):
         self.complimentList = ["Nice!", "Good", "Correct", "Ding Ding Ding", "Nice going", "That's right!"]
 
         self.wrongAnswerList = ["Sorry!", "Nope!", "Too bad"]
+
+        self.testList = ["Okay!, what level do you want to test?"]
 
     def savePracticeLevel(self, level):
         file = open("levelDB.txt", "a+")
@@ -268,6 +336,7 @@ class SampleApplication(Base.AbstractApplication):
         return list(set(levels))
 
     def onRobotEvent(self, event):
+        print(event)
         if event == 'LanguageChanged':
             self.langLock.release()
         elif event == 'TextDone':
@@ -276,14 +345,26 @@ class SampleApplication(Base.AbstractApplication):
             self.gestureLock.release()
 
     def onAudioIntent(self, *args, intentName):
+        print("INTENT NAME = " + intentName)
         if intentName == "answer_name" and len(args) > 0:
-            print("hey")
+            print(args[0])
             self.name = args[0]
             self.nameLock.release()
 
-        if intentName == "awaiting_practice_test" and len(args) > 0:
+        if intentName == "answer_practice_test" and len(args) > 0:
+            print(args[0])
             self.practiceTest = args[0]
             self.practiceTestLock.release()
+
+        if intentName == "answer_level" and len(args) > 0:
+            print(args[0])
+            self.level = args[0]
+            self.levelLock.release()
+
+        if intentName == "answer_answerLevelOne" and len(args) > 0:
+            print(args[0])
+            self.answer = args[0]
+            self.answerLock.release()
 
 
 # Run the application
